f057bde0eb3860a967f876d069584e48540e1edb
Claudio Vilas <claudio.vilas@eldars.local>
2023-03-07 18:04:43 -0300

    implementando seguridad

---
+++ src/main/java/com/caito/tutorialwebflux/security/entity/User.java
@@ -0,0 +1,61 @@
+package com.caito.tutorialwebflux.security.entity;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Table(name = "users")
+public class User implements UserDetails {
+    @Id
+    private Integer id;
+    private String username;
+    private String email;
+    private String password;
+    private String roles;
+
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Stream.of(roles.split(", ")).map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+}
---
+++ src/main/java/com/caito/tutorialwebflux/security/repository/SecurityContextRepository.java
@@ -0,0 +1,32 @@
+package com.caito.tutorialwebflux.security.repository;
+
+import com.caito.tutorialwebflux.security.jwt.JwtAuthenticationManager;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextImpl;
+import org.springframework.security.web.server.context.ServerSecurityContextRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class SecurityContextRepository implements ServerSecurityContextRepository {
+    @Autowired
+    private JwtAuthenticationManager jwtAuthenticationManager;
+
+    @Override
+    public Mono<Void> save(ServerWebExchange exchange, SecurityContext context) {
+        return Mono.empty();
+    }
+
+    @Override
+    public Mono<SecurityContext> load(ServerWebExchange exchange) {
+        String token = exchange.getAttribute("token");
+        return jwtAuthenticationManager.authenticate(new UsernamePasswordAuthenticationToken(
+                token, token))
+                .map(SecurityContextImpl::new);
+    }
+}
